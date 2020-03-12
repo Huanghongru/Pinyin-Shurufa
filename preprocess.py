62fe9f7451dc61a24f9f02b698e3d1a8a3fd2689
huanghongru <onedesire@sjtu.edu.cn>
2020-03-12 18:49:02 +0800

    update preprocess

--- preprocess.py
+++ preprocess.py
@@ -24,7 +24,11 @@ def normalize(text):
         out_t = ""
         for kanji in t:
             out_t += kanji if kanji in kanji2pinyin.keys() else ' '
-        return out_t.split()
+        out = []
+        for span in out_t.split():
+            if len(span) > 1:
+                out.append(span)
+        return out
 
     texts = []
     for text in tmps:
@@ -47,22 +51,16 @@ def read_news(fname):
 def make_dataset():
     corpus = open("data/corpus.dat", "w")
 
-    total, bad_examples = 0., 0.
+    total = 0
     for data_file in os.listdir('data'):
         if data_file.split('.')[-1] != 'txt':
             continue
         texts = read_news(os.path.join('data', data_file))
         for text in texts:
-            if len(text) < 2:
-                bad_examples += 1
-                continue
             corpus.write("/" + text + "\n")
 
         total += len(texts)
-        print("preprocessed {}".format(data_file))
-    print("Total corpus: {}\t Bad examples: {} Effective rate: {}".format(
-        total, bad_examples, (total - bad_examples) / total))
-
+        print("preprocessed {}, line of corpus: {}".format(data_file, total))
     corpus.close()
 
 make_dataset()
